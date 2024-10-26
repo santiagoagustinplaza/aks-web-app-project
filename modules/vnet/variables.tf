ab8252ae347c158c944c2e5b5e22a9d76d73b926
Santiago Agustin Plaza <santiago.a.plaza@accenture.com>
2024-10-25 21:03:07 -0300

    require vnet name

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -1,7 +1,6 @@
-variable "vnet_name" { #Optional
+variable "vnet_name" {
   description = "Name of the Virtual Network"
   type        = string
-  default     = "aks-default-vnet"
 }
 
 variable "location" {
