eef400894860b4e03cbe992f384c9296115460f4
Santiago Agustin Plaza <santiago.a.plaza@accenture.com>
2024-10-27 18:42:21 -0300

    add connection string output value

--- modules/sql/outputs.tf
+++ modules/sql/outputs.tf
@@ -12,3 +12,8 @@ output "sql_private_endpoint_ip" {
   description = "The private IP address of the SQL Private Endpoint"
   value       = azurerm_private_endpoint.sql_private_endpoint.private_ip_address
 }
+
+output "sql_connection_string" {
+  value     = "Server=tcp:${azurerm_sql_server.sql_server.name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.sql_db.name};Persist Security Info=False;User ID=${var.sql_admin_user};Password=${var.sql_admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  sensitive = true
+}
