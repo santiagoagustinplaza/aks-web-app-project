c5d77eb7831df0f615a404b96c5939a5f3f28ffc
Santiago Agustin Plaza <santiago.a.plaza@accenture.com>
2024-10-30 16:33:05 -0300

    remove rg module

--- modules/rg/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  description = "The name of the Resource Group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_location" {
-  description = "The location of the Resource Group"
-  value       = azurerm_resource_group.main.location
-}
--- modules/rg/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "The name of the Resource Group"
-  type        = string
-}
-
-variable "location" {
-  description = "The Azure location where the Resource Group will be created"
-  type        = string
-}
